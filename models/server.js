cd2dc019d96e8450f363ae41394b2bc866f39ba1
Andres Chia <achia@soytul.com>
2022-08-14 22:18:39 -0500

    add env

--- models/server.js
+++ models/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 class Server {
   constructor() {
     this.app = express();
-    this.port = 3000;
+    this.port = process.env.PORT;
     this.dataPath = '/api';
 
     // Middlewares
